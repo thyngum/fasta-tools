a10922afc6c1dcee0ff33edeef20622f92645d6b
Alejandro Llanes <thyngum@gmail.com>
2016-07-03 11:17:22 -0500

    Added the fastq-to-* scripts

---
+++ fastq-to-fasta-qual.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+
+# Converts a FASTQ file to an equivalent pair of FASTA/QUAL files with the same 
+# basename of input file.
+
+#   fastq-to-fasta-qual [-f <format>] -in <filename>
+
+# 	-in	Input FASTQ file
+# 	-f 	Input file format: fastq (default), fastq-sanger, fastq-illumina or fastq-solexa.
+
+use Bio::SeqIO;
+use Bio::Seq::Quality;
+use File::Basename;
+use File::Spec;
+use Getopt::Long;
+ 
+GetOptions ( 'in=s' => \$input, 'f=s' => \$format );
+
+die "Usage: fastq-to-fasta-qual.pl [-f <format>] -in <filename>\n" if ( ! $input );
+
+$format = 'fastq' if ( ! $format );
+
+$filename = File::Spec->rel2abs($input);
+die "File \'$filename\' not found.\n" if ( ! -e $filename );
+ 
+($name, $path, $suffix) = fileparse($filename, qr/\.[^.]*/);
+$fasta_file = $path . $name . ".fasta";
+$qual_file = $path . $name . ".qual";
+ 
+$in_seq_obj = Bio::SeqIO->new(-file   => $filename,
+                              -format => $format, );
+
+$out_fasta_obj = Bio::SeqIO->new(-file   => ">$fasta_file",
+                                 -format => 'fasta', );
+
+$out_qual_obj = Bio::SeqIO->new(-file   => ">$qual_file",
+                                -format => 'qual', );
+
+while ( $seq = $in_seq_obj->next_seq ) {
+	$out_fasta_obj->write_seq($seq);
+    $out_qual_obj->write_seq($seq);
+    $count++;
+}
---
+++ fastq-to-fasta.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+
+# Converts a FASTQ file to FASTA, discarding quality info. Intended to be used
+# as a more efficient alternative to fastq-to-fasta-qual.pl for large FASTQ files.
+
+#   fastq-to-fasta.pl -in <filename> [-out <output>]
+
+# 	-in 	Input FASTQ file
+#	-out 	Ouput filename (outputs to SDTOUT by default)
+
+use Bio::Seq;
+use Bio::SeqIO;
+use File::Spec;
+use Getopt::Long;
+use utils;
+
+GetOptions ( 'in=s' => \$input, 'out=s' => \$output );
+
+die "Usage: fastq-to-fasta.pl -in <filename> [-out <output>]\n" if ( ! $input );
+
+my $filename = File::Spec->rel2abs($input);
+die "File \'$filename\' not found.\n" if ( ! -e $filename );
+
+open IN, $filename or die("Could not open input file.\n");
+
+my $line_count = 0;
+my $count = 0;
+while ( my $line = <IN> )  {  
+	$line_count++;
+	my $start = substr $line, 0, 1;
+	my $dummy = substr $line, 0, 2;		
+	if ( ( $start eq '@' ) and ( $dummy ne '@@' ) ) {
+		my $desc = $line;
+		my $seq = <IN>;
+		my $plus = <IN>;
+		my $qual = <IN>;
+		chomp $desc;
+		chomp $seq;
+		chomp $plus;
+		chomp $qual;
+		$line_count = $line_count + 3;
+		if ( $plus eq '+' ) {
+			$count++;
+			print ">" . substr($desc, 1) . "\n$seq\n";
+		}
+		else {
+			die "Incorrect FASTQ format around line " . ( $line_count - 1 ) . ".\n" ;	
+		}
+	}	
+}	
+close IN;
+
+print STDERR "Found $count reads in input file.\n";
