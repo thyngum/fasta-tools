2fa6a7f5322fa375370ece61aa30e47b864ba942
Alejandro Llanes <thyngum@gmail.com>
2016-07-02 20:36:07 -0500

    Removed the -aa flag, sequence type is now guessed from input

--- fasta-union.pl
+++ fasta-union.pl
@@ -2,14 +2,13 @@
 
 # Concatenates all the sequences in a multi-FASTA file. 
 
-#   fasta-union.pl [-aa] [-gap <n>] [-t] -in <filename> [-out <output>]
+#   fasta-union.pl [-gap <n>] [-t] -in <filename> [-out <output>]
 
 # 	-in 	Input multi-FASTA file
 #	-out 	Ouput filename (outputs to SDTOUT by default)
-#	-aa	Input sequences are amino acid sequences (default is nucleotide)
 # 	-gap	Add a gap of <n> Ns or Xs between each individual sequence.
-# 	-t	Write a table file (.tab) in EMBL format with the coordinates of
-# 		of the original sequences (only for nucleotide sequences).
+# 	-t  	Write a table file (.tab) in EMBL format with the coordinates of
+# 	    	of the original sequences (only for nucleotide sequences).
 
 use Bio::Seq;
 use Bio::SeqIO;
@@ -18,15 +17,13 @@ use File::Spec;
 use Getopt::Long;
 use utils;
 
-GetOptions ( 'in=s' => \$input, 'out=s' => \$output, 't' => \$tab, 'aa' => \$aa, 'gap=i' => \$gap );
+GetOptions ( 'in=s' => \$input, 'out=s' => \$output, 't' => \$tab, 'gap=i' => \$gap );
 
-die "Usage: fasta-union.pl [-aa] [-gap <n>] [-t] -in <filename> [-out <output>]\n" if ( ! $input );
-
-die "Flags -aa and -t cannot be used together!\n" if ( $tab && $aa );
+die "Usage: fasta-union.pl [-gap <n>] [-t] -in <filename> [-out <output>]\n" if ( ! $input );
 
 my $filename = File::Spec->rel2abs($input);
 die "File \'$filename\' not found.\n" if ( ! -e $filename );
-(my $name, my $path, my $suffix) = fileparse($filename, qr/\.[^.]*/);
+my ($name, $path, $suffix) = fileparse($filename, qr/\.[^.]*/);
 
 my $seqio_object = Bio::SeqIO->new(-file => $filename, 
                                 -format => 'fasta' ) or die $!;
@@ -39,68 +36,71 @@ if ( $output ) {
 if ( $tab ) {
 	$tab_file = $path . $name . ".tab";
 }
-if ( $gap ) {
-	if ( $aa ) {
-		$stretch = "X" x int($gap);
-	}
-	else {
-		$stretch = "N" x int($gap);
-	}	
-}                         
+                    
 
 my $count = 0;
-my $base_count = 0;
-my $union = "";
-my $first = 1;
+my $union_seq = "";
+my $union_length = 0;
 my $tab_str = "";
+my $type;
+my $protein_gap = "X" x int($gap);
+my $nucl_gap = "N" x int($gap);
+
 while ( my $seq = $seqio_object->next_seq ) {
+	$count++;
+
+	if ( $count == 1 ) {
+		$type = $seq->alphabet();
+		die "The -t flag cannot be used with protein sequences!\n" if ( $type eq 'protein' and $tab );
+	}
+	
 	if ( $gap ) {
-		if ( $first ) {
-			$union = $union . $seq->seq();
-			$first = 0;
+		unless ( $count == 1 ) {
+			if ( $type eq 'protein' ) {
+				$union_seq = $union_seq . $protein_gap . $seq->seq();
+			}
+			else {
+				$union_seq = $union_seq . $nucl_gap . $seq->seq();
+			}			
+			$union_length += $gap;
 		}
 		else {
-			$union = $union . $stretch . $seq->seq();
-			$base_count = $base_count + $gap;
+			$union_seq .= $seq->seq();
 		}
 	}
-	else { 
-		$union = $union . $seq->seq();		
+	else {
+		$union_seq .= $seq->seq();		
 	}
-	my $length = $seq->length;	
 
 	if ( $tab ) {
-		$start = $base_count + 1;
-		$end = $base_count + $length;	
+		$start = $union_length + 1;
+		$end = $union_length + $seq->length();	
 		$tab_str = $tab_str . "FT   misc_feature    $start..$end\n";
 		$tab_str = $tab_str . "FT                   /note=\"" . $seq->display_id() . "\"\n";	
-		$base_count = $base_count + $length;	
 	}
 
-	$count++;
+	$union_length += $seq->length();
 }	
 
 if ( $output ) {
 	$seq_union = Bio::Seq->new(-display_id => $name, 
-	                           -seq => $union ) or die $!;
+	                           -seq => $union_seq ) or die $!;
 	$seqio_out->write_seq($seq_union);	
 }
 else {
 	print ">$name\n";
-	print sblock($union);	
+	print sblock($union_seq);	
 }
 
 if ( $tab ) {
 	open TAB, ">$tab_file";
-
-	# Print the EMBL header here
-	print TAB "ID   $name; SV 1; linear; unassigned DNA; STD; UNC; $base_count BP.\n";
-	print TAB "XX\n";
-	print TAB "AC   $name\n";
-	print TAB "XX\n";
-	print TAB "XX\n";
-	print TAB "XX\n";
-	print TAB "FH   Key             Location/Qualifiers\n";
-	print TAB $tab_str;
+	print TAB "ID   $name; SV 1; linear; unassigned DNA; STD; UNC; $union_length BP.\n",
+	  "XX\n",
+	  "AC   $name\n",
+	  "XX\n",
+	  "XX\n",
+	  "XX\n",
+	  "FH   Key             Location/Qualifiers\n",
+	  $tab_str;
 	close TAB
 }
