8285259f9407c6a3cb93f3418973ba482835fb22
Alejandro Llanes <thyngum@gmail.com>
2016-07-02 19:43:26 -0500

    Added fasta-stat.pl

--- fasta-stats.pl
+++ fasta-stats.pl
@@ -6,10 +6,10 @@
 # 	fasta-stats.pl [-f <format>] [-d] [-p] -in <filename>
 
 #   	-f   	Input format: fasta, fastq, genbank, etc. (guessed if omitted)
-# 		-c      Clean output (minimum descriptions, suitable for redirection)
-#   	-d		Detailed stats (including base counts)
-#   	-p		Also prints a tab-separated list with description and length of 
-# 				individual sequences to STDOUT.
+# 	-c      Clean output (minimum descriptions, suitable for redirection)
+#   	-d	Detailed stats (including base counts)
+#   	-p	Also prints a tab-separated list with description and length of 
+# 		individual sequences to STDOUT.
 
 use Bio::Seq;
 use Bio::SeqIO;
