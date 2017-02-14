e8eba1e8b1919c9e19a9579273c456b4c48bb32c
Alejandro Llanes <thyngum@gmail.com>
2017-02-14 08:46:35 -0500

    Major revision

--- fasta-extract.pl
+++ fasta-extract.pl
@@ -9,9 +9,9 @@
 #    -l <list>	Tab-delimited file with a subsequence specification per line, 
 #             	with the format:
 #
-#            	ID	SOURCE	START	END
+#            	SOURCE  [ID  START  END]
 #
-#            	Where SOURCE is a sequence in input file, and ID, START and END are
+#            	Where SOURCE is a sequence in input file and ID, START and END are
 #            	the name and coordinates of the subsequence to be extracted from SOURCE.
 #            	If start > end the subsequence is extracted from the reverse strand 
 #            	of SOURCE (but using the positions of the forward strand). If no
@@ -35,62 +35,70 @@ use utils;
 
 GetOptions ( 'in=s' => \$input, 'l=s' => \$list, 'out=s' => \$output );
 
-die "Usage: fasta-extract.pl -l <list> -in <filename> [-out <output>].\n" if ( (! $input) || (! $list) );
+die "Usage: fasta-extract.pl -l <list> -in <filename> [-out <output>].\n" unless ( $input and $list );
 
-$filename = File::Spec->rel2abs($input);
+my $filename = File::Spec->rel2abs($input);
 die "File \'$filename\' not found.\n" if ( ! -e $filename );
 
-$listfile = File::Spec->rel2abs($list);
+my $listfile = File::Spec->rel2abs($list);
 die "File \'$listfile\' not found.\n" if ( ! -e $listfile );
 
 if ( $output ) {
-	$output_filename = File::Spec->rel2abs($output);
-	$seqio_out = Bio::SeqIO->new(-file => ">$output_filename", 
+	$seqio_out = Bio::SeqIO->new(-file => ">$output", 
 	                             -format => 'fasta') or die $!;	
 }
 
-# Load input file in memory and open output file
-%seqs = ();
-
-$seqio_object = Bio::SeqIO->new(-file => $filename, 
-                                -format => $format ) or die $!;								    
-
-while ( $seq = $seqio_object->next_seq ) {
-	$seqs{$seq->display_id()} = $seq->seq;
+# Hash input sequences
+my %seqs = ();
+my $seqio_in = Bio::SeqIO->new(-file => $filename, 
+                            -format => $format ) or die $!;								    
+while ( my $seq = $seqio_in->next_seq ) {
+	$seqs{$seq->display_id} = $seq->seq;
 }							    
 
+# Process list file
 open LIST, $listfile;
 
-while ( $line = <LIST> ) {
+while ( my $line = <LIST> ) {
 	chomp $line;
-	if ( $line ne "" ) {		
+	if ( $line ne "" ) {
 
-		( $id, $source, $start, $end ) = split("\t", $line);
+		( my $source, my $id, my $start, my $end ) = split /\t/, $line;
 
-		unless ( $seqs{$source} ) {
-			print STDERR "Warning: No sequece with ID \'$source\' in input file!\n";
+		if ( $id or $start or $end ) {
+			unless ( $id and $start and $end ) {
+				die "Format of list file is: SOURCE  [ID  START  END]!\n";
+			}	
 		}
-		else {
-			$seq_obj = Bio::Seq->new(-seq => $seqs{$source} );
-			if ( ! $start && ! $end ) {
-				$seq_obj-> display_id($id);
-				print ">$id\n"; ### TODO: Check if an ID is already in ouput, to avoid duplicate IDs!
-				print sblock($seq_obj->seq());
-				$seqio_output->write_seq($seq_obj) if ( $output );						
+
+		if ( $seqs{$source} ) {			
+			my $tmp_seq = Bio::Seq->new(-seq => $seqs{$source});			                            
+			unless ( $start and $end ) {
+				if ( $output ) {
+					$tmp_seq->display_id($source);
+					$seqio_out->write_seq($tmp_seq);
+				}
+				else {
+					print ">$source\n"; ### TO-DO: Check if an ID is already in ouput, to avoid duplicate IDs!
+					print sblock( $seqs{$source} );
+				}
 			}
 			else {
 				if ( $start <= $end ) {
-					if ( $start > 0 and $end <= length($seq_obj->seq()) ) {
-						$subseq = $seq_obj->subseq($start, $end);
-						$subseq_obj = Bio::Seq->new(-seq => $subseq,
-						                            -display_id => "$id",
-						                            -desc => "    $source:$start..$end" );	
-						print ">" . $subseq_obj->display_id() . $subseq_obj->desc() . "\n";
-						print sblock($subseq_obj->seq());									
-						$seqio_output->write_seq($subseq_obj) if ( $output );
+					if ( $start > 0 and $end <= length($tmp_seq->seq()) ) {
+						$subseq = Bio::Seq->new(-seq => $tmp_seq->subseq($start, $end),
+						                        -display_id => $id,
+						                        -desc => "\t$source:$start..$end" );	
+						if ( $output ) {
+							$seqio_out->write_seq($subseq);
+						}
+						else {
+							print ">$id\t$source:$start..$end\n";
+							print sblock($subseq->seq());								
+						}
 					}
 					else {
-						print STDERR "Warning: sequence coordinates out of range for $id!\n";
+						print STDERR "Warning: sequence coordinates for $id are out of range!\n";
 					}					
 				}
 				else {
@@ -102,18 +110,24 @@ while ( $line = <LIST> ) {
 					#  rev = length of the original sequence - start/end + 1
 
 					# TODO: Check validity of coordinates for subsequences!
+					$subseq = Bio::Seq->new(-seq => $tmp_seq->subseq($end, $start)->revcom->seq,
+					                        -display_id => $id,
+					                        -desc => "\t$source:complement($c_start..$c_end)" );					
+					my $c_start = $tmp_seq->length - $start + 1;
+					my $c_end = $tmp_seq->length - $end + 1;
 
-					$subseq = $seq_obj->subseq($end, $start); 
-					$tmp_obj = Bio::Seq->new(-seq => $subseq );
-					$subseq_obj = $tmp_obj->revcom;
-					$subseq_obj->display_id($id);
-					$c_start = $seq_obj->length - $start + 1;
-					$c_end = $seq_obj->length - $end + 1;
-					$subseq_obj->desc("    $source:complement($c_start..$c_end)");
-					print sblock($subseq_obj->seq());
-					$seqio_output->write_seq($subseq_obj) if ( $output );					
+					if ( $output ) {
+						$seqio_out->write_seq($subseq_obj) if ( $output );
+					}
+					else {
+						print ">$id\t$source:complement($c_start..$c_end)\n";
+						print sblock($subseq_obj->seq());
+					}					
 				}
 			}
+		}
+		else {
+			print STDERR "Warning: Could not find a sequence \'$source\' in input file!\n";
 		}
 	}
 }	
