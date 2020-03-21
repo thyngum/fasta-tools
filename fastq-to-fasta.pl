0e2c8c5419aa66053bdc42ce29a631a560ef9b7a
thyngum <thyngum@gmail.com>
2020-03-21 11:15:59 -0500

    Adds fastq-to-fasta.pl

--- fastq-to-fasta.pl
+++ fastq-to-fasta.pl
@@ -1,15 +1,14 @@
 #!/usr/bin/perl
 
-# Converts a FASTQ file to FASTA, discarding quality info. Intended to be used
-# as a more efficient alternative to fastq-to-fasta-qual.pl for large FASTQ files.
+# Converts a FASTQ file to FASTA format, discarding quality info. 
 
 #   fastq-to-fasta.pl -in <filename> [-out <output>]
 
 # 	-in 	Input FASTQ file
 #	-out 	Ouput filename (outputs to SDTOUT by default)
 
-use Bio::Seq;
-use Bio::SeqIO;
+# Script written by Alejandro Llanes (thyngum@gmail.com)
+
 use File::Spec;
 use Getopt::Long;
 
@@ -25,33 +24,52 @@ die "Usage: fastq-to-fasta.pl -in <filename> [-out <output>]\n" if ( ! $input );
 my $filename = File::Spec->rel2abs($input);
 die "File \'$filename\' not found.\n" if ( ! -e $filename );
 
-open IN, $filename or die("Could not open input file.\n");
+if ( $output ) {
+	my $output_filename = File::Spec->rel2abs($output);
+	open OUT, ">$output_filename";
+}
+
+open IN, $filename or die "File '$filename' not found.\n";
 
 my $line_count = 0;
-my $count = 0;
 while ( my $line = <IN> )  {  
-	$line_count++;
-	my $start = substr $line, 0, 1;
-	my $dummy = substr $line, 0, 2;		
-	if ( ( $start eq '@' ) and ( $dummy ne '@@' ) ) {
-		my $desc = $line;
-		my $seq = <IN>;
-		my $plus = <IN>;
-		my $qual = <IN>;
-		chomp $desc;
-		chomp $seq;
-		chomp $plus;
-		chomp $qual;
-		$line_count = $line_count + 3;
-		if ( $plus eq '+' ) {
-			$count++;
-			print ">" . substr($desc, 1) . "\n$seq\n";
-		}
-		else {
-			die "Incorrect FASTQ format around line " . ( $line_count - 1 ) . ".\n" ;	
-		}
-	}	
+	my $desc = $line;
+	my $seq = <IN>;
+	my $plus = <IN>;
+	my $qual = <IN>;
+
+	$line_count = $line_count + 4;
+
+	die "Incorrect FASTQ format around line $line_count\n" unless ( $desc =~ /^\@/ and $plus =~ /^\+/ ); 
+
+	chomp $desc;
+	chomp $seq;
+	chomp $plus;
+	chomp $qual;
+
+	( my $id ) = split /\s/, $desc;
+	$id = substr $id, 1;
+
+	if ( $output ) {
+		print OUT ">$id\n";
+		print OUT sblock($seq);	
+	}
+	else {
+		print ">$id\n";
+		print sblock($seq);
+	}
+
 }	
 close IN;
 
-print STDERR "Found $count reads in input file.\n";
+if ( $output ) {
+	close OUT;
+}
+
+$count = $line_count/4;
+
+unless ( $output ) {
+	print "\n";
+}
+
+print STDERR "Found $count sequences in input file.\n";
